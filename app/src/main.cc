cf46ac448324d33b42bfb76fecc64f3881275587
Yuval Peress <peress@google.com>
2023-03-10 10:40:50 -0700

    Use tokenized logging
    Demonstrate both Zephyr and Pigweed logging go through the same pipeline

--- app/src/main.cc
+++ app/src/main.cc
@@ -1,8 +1,10 @@
 #define PW_LOG_MODULE_NAME "APP"
 
+#include <zephyr/logging/log.h>
 #include <pw_log/log.h>
 
 int main(void) {
-  PW_LOG_INFO("Hello");
+  PW_LOG_INFO("Hello Pigweed logging");
+  LOG_INF("Hello Zephyr logging");
   return 0;
 }
