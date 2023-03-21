fe53dbabbfa77e71129484af9023533c22b464d3
Al Semjonovs <asemjonovs@google.com>
2023-03-21 15:57:42 -0600

    WIP: tokenize changes
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>

--- app/src/main.cc
+++ app/src/main.cc
@@ -1,10 +1,23 @@
 #define PW_LOG_MODULE_NAME "APP"
 
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <pw_log/log.h>
 
 int main(void) {
-  PW_LOG_INFO("Hello Pigweed logging");
-  LOG_INF("Hello Zephyr logging");
+  while(1) {
+
+    PW_LOG_DEBUG("Hello Pigweed DEBUG");
+    PW_LOG_INFO("Hello Pigweed INFO");
+    PW_LOG_WARN("Hellow Pigweed WARN");
+    PW_LOG_ERROR("Hello Pigweed ERROR");
+
+    LOG_ERR("Hello Zephyr ERR");
+    LOG_WRN("Hello Zephyr WRN");
+    LOG_INF("Hello Zephyr INF");
+    LOG_DBG("Hello Zephyr DBG");
+
+    k_sleep(K_MSEC(1000));
+  }
   return 0;
 }
