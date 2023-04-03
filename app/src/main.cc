45eb6a2568a3b0f6cbed45d3adef4e58472a1eb8
Yuval Peress <peress@google.com>
2023-04-03 00:20:13 -0600

    Add Pigweed logging

--- app/src/main.cc
+++ app/src/main.cc
@@ -3,13 +3,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define PW_LOG_MODULE_NAME "APP"
+
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-
-LOG_MODULE_REGISTER(app, CONFIG_APP_LOG_LEVEL);
+#include <pw_log/log.h>
 
 int main(void) {
   while (true) {
+    PW_LOG_DEBUG("Hello Pigweed DEBUG");
+    PW_LOG_INFO("Hello Pigweed INFO");
+    PW_LOG_WARN("Hello Pigweed WARN");
+    PW_LOG_ERROR("Hello Pigweed ERROR");
+
     LOG_DBG("Hello Zephyr DBG");
     LOG_INF("Hello Zephyr INF");
     LOG_WRN("Hello Zephyr WRN");
